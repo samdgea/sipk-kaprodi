a335b6186b7e5a336ca8b5b7551f0c19f63c6b03
Abdilah Sammi <43635222+samdgea@users.noreply.github.com>
2021-08-18 14:43:38 +0700

    Update ProfileInformationTest.php

--- tests/Feature/ProfileInformationTest.php
+++ tests/Feature/ProfileInformationTest.php
@@ -18,7 +18,8 @@ class ProfileInformationTest extends TestCase
 
         $component = Livewire::test(UpdateProfileInformationForm::class);
 
-        $this->assertEquals($user->name, $component->state['name']);
+        $this->assertEquals($user->first_name, $component->state['first_name']);
+        $this->assertEquals($user->last_name, $component->state['last_name']);
         $this->assertEquals($user->email, $component->state['email']);
     }
 
@@ -27,10 +28,11 @@ class ProfileInformationTest extends TestCase
         $this->actingAs($user = User::factory()->create());
 
         Livewire::test(UpdateProfileInformationForm::class)
-                ->set('state', ['name' => 'Test Name', 'email' => 'test@example.com'])
+                ->set('state', ['first_name' => 'Test','last_name' => 'User' , 'email' => 'test@example.com'])
                 ->call('updateProfileInformation');
 
-        $this->assertEquals('Test Name', $user->fresh()->name);
+        $this->assertEquals('Test', $user->fresh()->first_name);
+        $this->assertEquals('Name', $user->fresh()->last_name);
         $this->assertEquals('test@example.com', $user->fresh()->email);
     }
 }
